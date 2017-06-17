ed1f1bdfd07e505b515941dd67fd9621c814cb36
Bogdan Orzea <bogdan.orzea@gmail.com>
2017-06-17 22:56:18 +0300

    Implement AsyncTask and JSON now served from server

--- app/src/main/java/com/bogdanorzea/regexquiz/MainActivity.java
+++ app/src/main/java/com/bogdanorzea/regexquiz/MainActivity.java
@@ -1,5 +1,6 @@
 package com.bogdanorzea.regexquiz;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
@@ -14,16 +15,18 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
 
 import static com.bogdanorzea.regexquiz.R.layout.activity_main;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String LOG_TAG = MainActivity.class.getName();
     private static final String PROGRESS = "PROGRESS";
     private static final String CORRECT_ANSWERS = "CORRECT_ANSWERS";
     private static Bundle mBundleRecyclerViewState;
@@ -51,19 +54,18 @@ public class MainActivity extends AppCompatActivity {
             progressStatus = savedInstanceState.getInt(PROGRESS);
             correctAnswers = savedInstanceState.getInt(CORRECT_ANSWERS);
             mAdapter = (QuestionAdapter) savedInstanceState.getSerializable(ADAPTER);
+            setAdapter();
         } else {
             reinitializeProgress();
         }
-        setAdapter();
     }
 
     /**
      * Sets the initial values for the quiz
      */
     void reinitializeProgress() {
-        progressStatus = 0;
-        correctAnswers = 0;
-        mAdapter = new QuestionAdapter(generateQuestions());
+        // TODO sharedPreferences
+        new GetQuestionsTask().execute("192.168.43.86", "123");
     }
 
     /**
@@ -89,84 +91,11 @@ public class MainActivity extends AppCompatActivity {
                         progressFragment.title = getString(R.string.congratulations);
                         progressFragment.message = String.format(getString(R.string.progress_status), correctAnswers, progressStatus);
                         progressFragment.show(getFragmentManager(), "Congratulation Fragment");
+                        new PutResultsTask().execute("192.168.43.86", "123", String.format("%1$d/%2$d", correctAnswers, progressStatus));
                     }
                 }
             });
         }
-    }
-
-    /**
-     * This function generates the Question objects by parsing an JSON file
-     *
-     * @return List of Question object
-     */
-    private List<Question> generateQuestions() {
-        // JSON Node names
-        String TAG_QUESTIONS = "questions";
-        String TAG_TITLE = "title";
-        String TAG_DESCRIPTION = "description";
-        String TAG_CHOICES = "choices";
-        String TAG_ANSWERS = "answers";
-
-        // Read JSON file
-        StringBuilder jsonString = new StringBuilder();
-        BufferedReader in = null;
-        try {
-            InputStream json = getAssets().open("questions.json");
-            in = new BufferedReader(new InputStreamReader(json, "UTF-8"));
-            String str;
-
-            while ((str = in.readLine()) != null) {
-                jsonString.append(str);
-            }
-        } catch (IOException e) {
-            Log.e("JSON", "Error opening asset questions.json");
-        } finally {
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (IOException e) {
-                    Log.e("JSON", "Error closing asset questions.json");
-                }
-            }
-        }
-
-        List<Question> qList = new ArrayList<>();
-
-        try {
-            JSONObject jsonObj = new JSONObject(jsonString.toString());
-
-            JSONArray jsonQuestions = jsonObj.getJSONArray(TAG_QUESTIONS);
-            for (int i = 0; i < jsonQuestions.length(); i++) {
-                String title;
-                String description;
-                String[] choices;
-                String[] answers;
-
-                JSONObject q = jsonQuestions.getJSONObject(i);
-
-                title = q.getString(TAG_TITLE);
-                description = q.getString(TAG_DESCRIPTION);
-                JSONArray jsonChoices = q.getJSONArray(TAG_CHOICES);
-                choices = new String[jsonChoices.length()];
-                for (int j = 0; j < jsonChoices.length(); j++) {
-                    choices[j] = jsonChoices.getString(j);
-                }
-                JSONArray jsonAnswers = q.getJSONArray(TAG_ANSWERS);
-                answers = new String[jsonAnswers.length()];
-                for (int j = 0; j < jsonAnswers.length(); j++) {
-                    answers[j] = jsonAnswers.getString(j);
-                }
-
-                Question temp = new Question(title, description, choices, answers);
-                qList.add(temp);
-
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-            return null;
-        }
-        return qList;
     }
 
     @Override
@@ -230,5 +159,123 @@ public class MainActivity extends AppCompatActivity {
 
     void notifyDataSetChanged() {
         mAdapter.notifyDataSetChanged();
+    }
+
+    class GetQuestionsTask extends AsyncTask<String, Void, String> {
+        public final String LOG_TAG = GetQuestionsTask.class.getName();
+
+        @Override
+        protected void onPostExecute(String s) {
+            // Reset game status
+            progressStatus = 0;
+            correctAnswers = 0;
+            if (!s.isEmpty()) {
+                mAdapter = new QuestionAdapter(generateQuestions(s));
+                setAdapter();
+            }
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String response = "";
+            try {
+                if (InetAddress.getByName(strings[0]).isReachable(1000)) {
+                    Socket clientSocket = null;
+                    DataInputStream dataInputStream = null;
+
+                    clientSocket = new Socket(strings[0], Integer.parseInt(strings[1]));
+                    dataInputStream = new DataInputStream(clientSocket.getInputStream());
+
+                    if (dataInputStream != null) {
+                        response = dataInputStream.readUTF();
+                    }
+
+                } else {
+                    Log.i(LOG_TAG, "Server not reachable!");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return response;
+        }
+
+        /**
+         * This function generates the Question objects by parsing an JSON file
+         *
+         * @return List of Question object
+         */
+        private List<Question> generateQuestions(String s) {
+            // JSON Node names
+            String TAG_QUESTIONS = "questions";
+            String TAG_TITLE = "title";
+            String TAG_DESCRIPTION = "description";
+            String TAG_CHOICES = "choices";
+            String TAG_ANSWERS = "answers";
+
+            List<Question> qList = new ArrayList<>();
+
+            try {
+                JSONObject jsonObj = new JSONObject(s);
+
+                JSONArray jsonQuestions = jsonObj.getJSONArray(TAG_QUESTIONS);
+                for (int i = 0; i < jsonQuestions.length(); i++) {
+                    String title;
+                    String description;
+                    String[] choices;
+                    String[] answers;
+
+                    JSONObject q = jsonQuestions.getJSONObject(i);
+
+                    title = q.getString(TAG_TITLE);
+                    description = q.getString(TAG_DESCRIPTION);
+                    JSONArray jsonChoices = q.getJSONArray(TAG_CHOICES);
+                    choices = new String[jsonChoices.length()];
+                    for (int j = 0; j < jsonChoices.length(); j++) {
+                        choices[j] = jsonChoices.getString(j);
+                    }
+                    JSONArray jsonAnswers = q.getJSONArray(TAG_ANSWERS);
+                    answers = new String[jsonAnswers.length()];
+                    for (int j = 0; j < jsonAnswers.length(); j++) {
+                        answers[j] = jsonAnswers.getString(j);
+                    }
+
+                    Question temp = new Question(title, description, choices, answers);
+                    qList.add(temp);
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                return null;
+            }
+            return qList;
+        }
+    }
+
+    class PutResultsTask extends AsyncTask<String, Void, String> {
+        public final String LOG_TAG = PutResultsTask.class.getName();
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String response = "";
+            try {
+                if (InetAddress.getByName(strings[0]).isReachable(1000)) {
+                    Socket clientSocket = null;
+                    DataOutputStream dataOutputStream = null;
+
+                    clientSocket = new Socket(strings[0], Integer.parseInt(strings[1]));
+                    dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());
+
+                    dataOutputStream.writeUTF(strings[2]);
+
+                } else {
+                    Log.i(LOG_TAG, "Server not reachable!");
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return response;
+        }
     }
 }
